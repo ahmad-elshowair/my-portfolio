4316585ea15dce51129d5e416757f39e7a56a1c8
Ahmad Elshowair <ahmad-elshowair.dev@outlook.com>
2025-02-08 12:47:25 +0700

    feat: Add navigation components and update UI configuration
    - Add Navbar component for site navigation
    - Create reusable button UI component
    - Update layout and page components
    - Modify next.config and tailwind configuration
    - Update package dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "@/components/Navbar";
+
 export default function Home() {
 	return (
-		<main className="flex items-center justify-center h-screen">
+		<>
+		<Navbar/>
+		<main className="flex items-center justify-center h-[calc(100vh-70px)]">
 			<h1 className="text-4xl font-bold text-center">Hi, There !</h1>
 		</main>
+		</>
 	);
 }
